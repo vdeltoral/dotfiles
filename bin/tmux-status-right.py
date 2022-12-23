6f31268646e045b3b0feb3699391d5aa9b65ad2d
Vincent Del Toral <vdeltoral@gmail.com>
2022-12-23 17:16:16 -0600

    silencing tmux right status

--- bin/tmux-status-right.py
+++ bin/tmux-status-right.py
@@ -1,16 +1,16 @@
-import sys
-import os
+# import sys
+# import os
 
-sys.path.append(os.path.abspath("/Users/vincent/bin"))
-
-
-from publicip import get_public_ip
-from localip import get_local_ip
+# sys.path.append(os.path.abspath("/Users/vincent/bin"))
 
 
-public_ip = get_public_ip()
-local_ip = get_local_ip()
+# from publicip import get_public_ip
+# from localip import get_local_ip
 
-result = f"{public_ip}:{local_ip}"
 
-print(result)
+# public_ip = get_public_ip()
+# local_ip = get_local_ip()
+
+# result = f"{public_ip}:{local_ip}"
+
+# print(result)
