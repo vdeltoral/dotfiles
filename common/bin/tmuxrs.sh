0bd1125d7ae37acca268d8d7e1d5d9d0bc2d161d
Vincent Del Toral <vdeltoral@gmail.com>
2026-03-04 20:33:23 -0500

    ports program. tmux sessions

--- common/bin/tmuxrs.sh
+++ common/bin/tmuxrs.sh
@@ -4,11 +4,15 @@
 
 tmux kill-server > /dev/null 2>&1
 sleep 0.1
-tmux new -d -s "PRIMARY"
+tmux new -d -s "ONE"
 tmux new-window
 tmux new-window
 tmux next-window # go to first window
-tmux new -d -s "SECONDARY"
+tmux new -d -s "TWO"
+tmux new-window
+tmux new-window
+tmux next-window # go to first window
+tmux new -d -s "THREE"
 tmux new-window
 tmux new-window
 tmux next-window # go to first window
